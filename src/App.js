3e2a6021673c4b62a9bc1dd58020cace24e58fd2
Yihan Lu <yihanlu@Yihans-MacBook.local>
2018-06-18 22:21:47 +1000

    add edit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import SearchBar from './components/SearchBar';
 import CustomerTable from './components/CustomerTable';
+import EditForm from './components/EditForm'
 
 const DATA = [
   { "id": 1, "name": "James Bond", "email": "james@bond.com" },
@@ -12,6 +13,13 @@ const DATA = [
   { "id": 3, "name": "David Beckham", "email": "david@beckham.com" }
 ]
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeEditIndex: -1,
+    }
+  }
+
   onSearchClick = (e) => {
     console.log('On click')
   }
@@ -21,26 +29,51 @@ class App extends Component {
   }
 
   onClickEdit = (id) => {
-    console.log(id)
+    if(id === this.state.activeEditIndex) {
+      this.setState({activeEditIndex: -1})
+    } else {
+      this.setState({activeEditIndex: id})
+    }
+  }
+
+  onClickSave = (e, id) => {
+    console.log(e)
+    e.preventDefault();
+    console.log(id);
+    //save value
+    this.setState({activeEditIndex: -1})
   }
 
   render() {
+    const { activeEditIndex } = this.state;
+    const editing = activeEditIndex !== -1;
     return (
-      <div className="row">
-        <div className="column column-75">
+      <div className="container">
+        <div>
           <SearchBar
-            onSearchClick={this.onSearchClick}
-            onSearchInputChange={this.onSearchInputChange}
-            onClickEdit={this.onClickEdit}
-          />
-          <div>
-            <CustomerTable
-              data={DATA}
-              onClickEdit={this.onClickEdit}
-            />
-          </div>
+                onSearchClick={this.onSearchClick}
+                onSearchInputChange={this.onSearchInputChange}
+                onClickEdit={this.onClickEdit}
+              />
         </div>
-        <div className="column-25">something else</div>
+        <div className="row">
+          <div className={`column ${editing ? 'column-75' : 'column-100'}`}>
+            <div>
+              <CustomerTable
+                data={DATA}
+                onClickEdit={this.onClickEdit}
+              />
+            </div>
+          </div>
+          { editing &&
+            <div className="column">
+              <EditForm
+                id={1}
+                onClickSave={this.onClickSave}
+              />
+            </div>
+          }
+        </div>
       </div>
     );
   }
