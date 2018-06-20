93efa0c9bd817d3d25c64f9acc9a848e0591657f
Yihan Lu <yihanlu@Yihans-MacBook.local>
2018-06-20 23:06:10 +1000

    add in test

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     }
   }
 
-  componentWillMount = () => {
+  componentWillMount() {
     this.fetchAllCustomer()
   }
 
--- src/App.test.js
+++ src/App.test.js
@@ -4,41 +4,125 @@ import App from './App';
 import SearchBar from './components/SearchBar';
 import CustomerTable from './components/CustomerTable';
 import EditForm from './components/EditForm';
-import ErrorMsg from './components/ErrorMsg';
-import Sticky from './components/Sticky';
 
-it('App renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
+
+const DATA = [
+  { "id": 1, "name": "James Bond", "email": "james@bond.com" },
+  { "id": 2, "name": "David Jones", "email": "david@jones.com"  },
+  { "id": 3, "name": "David Beckham", "email": "david@beckham.com" }
+]
+
+configure({ adapter: new Adapter() });
+
+describe('render Customer Table', () => {
+  it('has 4 table row', () => {
+    const wrapper = shallow(
+      <CustomerTable
+        data={DATA}
+        onClickEdit={()=>{}}
+      />
+    );
+    expect(wrapper.find('tr')).toHaveLength(4);
+  });
+  it('has James Bond', () => {
+    const wrapper = shallow(
+      <CustomerTable
+        data={DATA}
+        onClickEdit={()=>{}}
+      />
+    );
+    expect(wrapper.contains(<td>James Bond</td>)).toEqual(true);
+  });
+  it('has david@jones.com', () => {
+    const wrapper = shallow(
+      <CustomerTable
+        data={DATA}
+        onClickEdit={()=>{}}
+      />
+    );
+    expect(wrapper.contains(<td>david@jones.com</td>)).toEqual(true);
+  });
 });
 
-it('SearchBar renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SearchBar />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('render Edit form', () => {
+  it('has 3 labels', () => {
+    const wrapper = shallow(
+      <EditForm
+        id={1}
+        onNameChange={()=>{}}
+        onEmailChange={()=>{}}
+        onClickSave={()=>{}}
+        name={'Yihan'}
+        email={'yihan.lu@outlook.com'}
+        error={{email: false, name: false}}
+      />
+    );
+    expect(wrapper.find('label')).toHaveLength(3);
+  });
+  it('has Yihan as a input value', () => {
+    const wrapper = shallow(
+      <EditForm
+        id={1}
+        onNameChange={()=>{}}
+        onEmailChange={()=>{}}
+        onClickSave={()=>{}}
+        name={'Yihan'}
+        email={'yihan.lu@outlook.com'}
+        error={{email: false, name: false}}
+      />
+    );
+    expect(wrapper.find({ value: 'Yihan' })).toHaveLength(1);
+  });
 });
 
-it('CustomerTable renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CustomerTable />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('render SearchBar', () => {
+  it('has a input with David as a input', () => {
+    const wrapper = shallow(
+      <SearchBar
+        searchTerm={'David'}
+        onSearchInputChange={()=>{}}
+        onSearchClick={()=>{}}
+      />
+    );
+    expect(wrapper.find({value: 'David'})).toHaveLength(1);
+  });
+  it('has a Search label', () => {
+    const wrapper = shallow(
+      <SearchBar
+        searchTerm={'David'}
+        onSearchInputChange={()=>{}}
+        onSearchClick={()=>{}}
+      />
+    );
+    expect(wrapper.contains(<span>Search</span>)).toEqual(true);
+  });
 });
 
-it('EditForm renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<EditForm />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('ErrorMsg renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ErrorMsg />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('Sticky renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Sticky />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('render Main app', () => {
+  it('mount without customer table', () => {
+    const wrapper = mount((
+      <App/>
+    ));
+    expect(wrapper.find(CustomerTable)).toHaveLength(0);
+  })
+  it('click on first edit button to open edit form', () => {
+    const wrapper = mount((
+      <App/>
+    ));
+    wrapper.setState({customers: DATA, loading: false});
+    wrapper.find('button').first().simulate('click');
+    expect(wrapper.find(EditForm)).toHaveLength(1);
+  })
+  it('double click on first edit button to close edit form', () => {
+    const wrapper = mount((
+      <App/>
+    ));
+    wrapper.setState({customers: DATA, loading: false});
+    wrapper.find('button').first().simulate('click');
+    wrapper.find('button').first().simulate('click');
+    expect(wrapper.find(EditForm)).toHaveLength(0);
+  })
 });
